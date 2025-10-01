c9246b4dacc405f6310195dbc7ceed1f3395748b
Alexa4 <alexa.carolina4497@gmail.com>
2025-09-30 23:09:20 -0500

    agrego funcion de inicio al sistema

--- funciones1.cpp
+++ funciones1.cpp
@@ -1,30 +1,55 @@
-#include <iostream>
-#include <string>
-#include <cstdlib>
-
-using namespace std;
+#include "librerias.h"
 
 int inicioSistema() {
+    // --- Sección Semilla n ---
     bool entrada_valida = false;
     string input_str;
-    int n;
+    int n = -1;
+
     while (!entrada_valida) {
-        cout << "Ingrese la semilla de codificación (n - número entero positivo): ";
+        cout << "Ingrese la semilla de codificacion n : ";
 
         if (!(cin >> input_str)) {
-            cin.clear();
             continue;
         }
-
-        n = atoi(input_str.c_str());
-
-        if (n > 0) {
+        try {
+            n = stoi(input_str);
+            if (n <= 0) {
+                throw 1;
+            }
             entrada_valida = true;
             cout << "Semilla (n) guardada: " << n << endl;
-            return n;
-        } else {
-            cout << "Error de validación: La semilla debe ser un número entero positivo (n > 0)." << endl;
+
+        }
+        catch (int error_code) {
+            if (error_code == 1) {
+                cout << "Error de validacion: La semilla debe ser un numero entero positivo." << endl;
+            }
+        }
+        catch (const invalid_argument& e) {
+            cout << "Error: Valor incorrecto. Ingrese solo numeros enteros." << endl;
         }
     }
-    return -1;
+
+    // --- Sección Método de Codificación con Validación ---
+    string metodo ="";
+    bool metodo_valido = false;
+
+    while (!metodo_valido) {
+        cout << "Ingrese el metodo de codificacion deseado (1 o 2): ";
+        cin >> metodo;
+
+        if (metodo == "1"){
+            metodo_valido = true;
+        }else if (metodo == "2"){
+            metodo_valido = true;
+        } else {
+            cout << "Error: Metodo no valido. Ingrese '1' o '2'." << endl;
+        }
+    }
+    return n;
+}
+
+int menuPrincipal(){
+    cout << "--- CAJERO ELECTRÓNICO UdeA ---"<< endl;
 }
--- librerias.h
+++ librerias.h
@@ -1,4 +1,12 @@
 #ifndef LIBRERIAS_H
 #define LIBRERIAS_H
 
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
+int inicioSistema() ;
+
 #endif // LIBRERIAS_H
